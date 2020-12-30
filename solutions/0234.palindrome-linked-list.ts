3b80150d73ab24f0e217628fde1491148ab08073
Kazuki Yamada <koukun0120@gmail.com>
2020-12-31 04:04:31 +0900

    add: 0234

---
+++ solutions/0234.palindrome-linked-list.ts
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+class ListNode {
+  val: number
+  next: ListNode | null
+
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+  }
+}
+
+function isPalindrome(head: ListNode | null): boolean {
+  let numbers = [];
+  let current = head;
+
+  while (current !== null) {
+    numbers.push(current.val);
+    current = current.next;
+  }
+
+  const numbersLength = numbers.length;
+  const numbersHalfLength = Math.floor(numbersLength / 2);
+
+  for (let i = 0; i < numbersLength; i++) {
+    if (numbers[i] !== numbers[numbersLength -1 - i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
