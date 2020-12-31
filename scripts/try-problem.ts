613a694b40d88aea8b245ca0806634a94b1bc45f
Kazuki Yamada <koukun0120@gmail.com>
2020-12-31 13:50:59 +0900

    update: 問題作成を自動化

---
+++ scripts/try-problem.ts
@@ -0,0 +1,68 @@
+import axios from 'axios';
+import * as fs from 'fs';
+import * as path from 'path';
+
+interface StatStatusPair {
+  stat: {
+    question_id: number;
+    question__title: string;
+    question__title_slug: string;
+  };
+  difficulty: {
+    level: number;
+  };
+  paid_only: boolean;
+}
+
+const scriptArgs = process.argv.slice(2);
+const problemId: number | null = scriptArgs[0] ? parseInt(scriptArgs[0]) : null;
+
+if (problemId === null) {
+  throw new Error('Problem id is empty');
+}
+
+axios.get('https://leetcode.com/api/problems/algorithms/').then((response) => {
+  const algorithms = response.data;
+  const statStatusPairs: StatStatusPair[] = algorithms.stat_status_pairs;
+
+  let targetStatStatusPair: StatStatusPair | null = null;
+
+  // Search
+  for (const statStatusPair of statStatusPairs) {
+    if (statStatusPair.stat.question_id === problemId) {
+      targetStatStatusPair = statStatusPair;
+    }
+  }
+
+  // Not found
+  if (targetStatStatusPair === null) {
+    throw new Error(`Not found problem. id: ${problemId}`);
+  }
+
+  // Create TS file
+  const tsFileNameProblemId = ('000' + problemId).slice(-4);
+  const tsFileProblemNameSlug = targetStatStatusPair.stat.question__title_slug;
+  const tsFileName = `${tsFileNameProblemId}.${tsFileProblemNameSlug}.ts`;
+  const tsFilePath = path.resolve(__dirname, '../solutions/', tsFileName);
+  const isFileExist = fs.existsSync(tsFilePath);
+
+  if (!isFileExist) {
+    fs.writeFile(tsFilePath, '', (err) => {
+      if (err) {
+        throw err;
+      }
+
+      console.info(`
+        Created TypeScript file
+        - ProblemId: ${problemId}
+        - ProblemTitle: ${targetStatStatusPair.stat.question__title}
+        - ProblemTitleSlug: ${tsFileProblemNameSlug}
+        - DifficultyLevel: ${targetStatStatusPair.difficulty.level}
+        - FileName: ${tsFileName}
+        - FilePath: ${tsFilePath}
+      `);
+    });
+  } else {
+    throw new Error(`File already exist. fileName: ${tsFileName}`);
+  }
+});
