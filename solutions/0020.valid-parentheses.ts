413b56bef9974d3529955b15ddafef9f1c638d81
Kazuki Yamada <koukun0120@gmail.com>
2021-01-07 00:50:11 +0900

    update: 20

--- solutions/0020.valid-parentheses.ts
+++ solutions/0020.valid-parentheses.ts
@@ -1,15 +1,42 @@
 function isValid(text: string): boolean {
-  const parenPairMap = {
-    '(': ')',
-    '{': '}',
-    '[': ']',
+  const parenEndStartPairMap: { [key: string]: string } = {
+    ')': '(',
+    '}': '{',
+    ']': '[',
   };
+  const startParens = Object.values(parenEndStartPairMap);
+  const endParens = Object.keys(parenEndStartPairMap);
 
   const parenStack: string[] = [];
   const textLength = text.length;
 
   for (let i = 0; i < textLength; i++) {
-    // TODO: キーにスタックに積んでいく。キーになかったらスタックから取る
+    const textChar = text[i];
+    const isStartParen = startParens.includes(textChar);
+    const isEndParen = endParens.includes(textChar);
+
+    if (isStartParen) {
+      parenStack.push(textChar);
+      continue;
+    }
+
+    if (endParens.includes(textChar)) {
+      const lastStackedParen = parenStack[parenStack.length - 1] || null;
+
+      if (lastStackedParen !== null) {
+        if (parenEndStartPairMap[textChar] === lastStackedParen) {
+          parenStack.pop();
+        } else {
+          return false;
+        }
+      } else {
+        return false;
+      }
+    }
+  }
+
+  if (parenStack.length > 0) {
+    return false;
   }
 
   return true;
