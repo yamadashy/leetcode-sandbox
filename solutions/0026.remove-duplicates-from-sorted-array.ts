ef685fb5f45a7a6a7483a74c66690ecea4b6207b
Kazuki Yamada <koukun0120@gmail.com>
2021-01-09 01:37:16 +0900

    add: 26

---
+++ solutions/0026.remove-duplicates-from-sorted-array.ts
@@ -0,0 +1,19 @@
+function removeDuplicates(nums: number[]): number {
+  let prevValue = null;
+
+  for (let i = 0; i < nums.length; ) {
+    const currentValue = nums[i];
+
+    if (prevValue === currentValue) {
+      nums.splice(i, 1);
+    } else {
+      i++;
+    }
+
+    prevValue = currentValue;
+  }
+
+  return nums.length;
+}
+
+export {};
