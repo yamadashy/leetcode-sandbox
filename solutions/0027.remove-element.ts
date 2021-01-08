203ddda72b1462875ad73b4bff058363bdd12448
Kazuki Yamada <koukun0120@gmail.com>
2021-01-09 01:55:31 +0900

    add: 27

---
+++ solutions/0027.remove-element.ts
@@ -0,0 +1,15 @@
+function removeElement(nums: number[], val: number): number {
+  for (let i = 0; i < nums.length; ) {
+    const currentValue = nums[i];
+
+    if (val === currentValue) {
+      nums.splice(i, 1);
+    } else {
+      i++;
+    }
+  }
+
+  return nums.length;
+}
+
+export {};
