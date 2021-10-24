d5d3cc6adb2750f0e81b03d1e59c97680ebbc7cf
Kazuki Yamada <koukun0120@gmail.com>
2021-10-24 19:53:42 +0900

    update: 35

---
+++ solutions/0035.search-insert-position.ts
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/search-insert-position
+function searchInsert(nums: number[], target: number): number {
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+
+    if (target <= num) {
+      return i;
+    }
+  }
+
+  return nums.length;
+};
