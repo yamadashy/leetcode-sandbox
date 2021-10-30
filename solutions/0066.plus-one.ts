91a07fa1e0921adb35961640a5b62166f43e8870
Kazuki Yamada <koukun0120@gmail.com>
2021-10-30 23:17:50 +0900

    add: 66

---
+++ solutions/0066.plus-one.ts
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/plus-one
+function plusOne(digits: number[]): number[] {
+  const last = digits.slice(-1)[0];
+
+  for (let i = digits.length - 1; i > -1; i--) {
+    const num = digits[i];
+
+    if (num === 9) {
+      digits[i] = 0;
+
+      if (i === 0) {
+        digits.unshift(1);
+      }
+    } else {
+      digits[i]++;
+      break;
+    }
+  }
+
+  return digits;
+};
