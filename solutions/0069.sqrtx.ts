548f679c164fdce2636e92bdb342a2ee5f0e322f
Kazuki Yamada <koukun0120@gmail.com>
2021-10-31 22:48:41 +0900

    update: 69

--- solutions/0069.sqrtx.ts
+++ solutions/0069.sqrtx.ts
@@ -1,15 +1,11 @@
 // https://leetcode.com/problems/sqrtx
 function mySqrt(x: number): number {
-  if (x === 0) {
-    return 0;
-  }
-
   let tmp = x;
 
   while (true) {
     tmp = Math.floor(tmp / 2);
 
-    if (tmp * tmp < x) {
+    if (tmp * tmp <= x) {
       break;
     }
   }
