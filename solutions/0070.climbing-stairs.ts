8b543016549fa71f01b4cf27bc49ff7ee5d2e6f5
Kazuki Yamada <koukun0120@gmail.com>
2021-10-31 23:31:37 +0900

    70

---
+++ solutions/0070.climbing-stairs.ts
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/climbing-stairs
+const resultMap = [];
+
+function climbStairs(n: number): number {
+  return re(n);
+};
+
+function re(n: number): number {
+  if (n <= 2) {
+    return n;
+  }
+
+  if (resultMap[n]) {
+    return resultMap[n];
+  }
+
+  const result = re(n - 2) + re(n - 1);
+
+  resultMap[n] = result;
+
+  return result;
+}
