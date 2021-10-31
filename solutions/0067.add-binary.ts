478a35d791f6113b4ebe77dea3e268121798d8cf
Kazuki Yamada <koukun0120@gmail.com>
2021-10-31 20:51:27 +0900

    add: 67

---
+++ solutions/0067.add-binary.ts
@@ -0,0 +1,42 @@
+// https://leetcode.com/problems/add-binary
+function addBinary(a: string, b: string): string {
+  const aLen = a.length;
+  const bLen = b.length;
+  const maxLen = Math.max(aLen, bLen);
+  let carryVal = 0;
+  let resultArray = [];
+
+  for (let i = 0; i < maxLen; i++) {
+    let aIndex = aLen - 1 - i;
+    let bIndex = bLen - 1 - i;
+    let aVal = 0;
+    let bVal = 0;
+    let sumVal = 0;
+
+    if (aIndex > -1) {
+      aVal = parseInt(a[aIndex]);
+    }
+
+    if (bIndex > -1) {
+      bVal = parseInt(b[bIndex]);
+    }
+
+    sumVal = aVal + bVal + carryVal;
+
+    if (sumVal >= 2) {
+      carryVal = 1;
+    } else {
+      carryVal = 0;
+    }
+
+    sumVal %= 2;
+
+    resultArray.unshift(sumVal);
+  }
+
+  if (carryVal === 1) {
+    resultArray.unshift(1);
+  }
+
+  return resultArray.join('');
+};
