8b760adf2eb49e2799f824863be9e9276f772a71
Kazuki Yamada <koukun0120@gmail.com>
2021-11-02 02:00:38 +0900

    update: same tree

--- solutions/0100.same-tree.ts
+++ solutions/0100.same-tree.ts
@@ -1,5 +1,19 @@
 // https://leetcode.com/problems/same-tree
 
 function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+  const numsP: number[] = [];
+  const numsQ: number[] = [];
+  inorderTraversalLoop(numsP, p);
+  inorderTraversalLoop(numsQ, q);
   return JSON.stringify(p) === JSON.stringify(q);
+}
+
+function inorderTraversalLoop(nums: number[], node: TreeNode | null) {
+  if (node !== null) {
+    inorderTraversalLoop(nums, node.left);
+    nums.push(node.val);
+    inorderTraversalLoop(nums, node.right);
+  } else {
+    nums.push(null);
+  }
 }
