1cbd178f207c5788396863e01c6c170b24eb3f7a
Kazuki Yamada <koukun0120@gmail.com>
2021-11-03 20:24:20 +0900

    add: 104

---
+++ solutions/0104.maximum-depth-of-binary-tree.ts
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/maximum-depth-of-binary-tree
+
+function maxDepth(root: TreeNode | null): number {
+  return maxDepthLoop(root, 0);
+};
+
+function maxDepthLoop(node: TreeNode | null, depth: number): number {
+  if (node === null) {
+    return depth;
+  }
+
+  return Math.max(maxDepthLoop(node.left, depth), maxDepthLoop(node.right, depth)) + 1;
+}
