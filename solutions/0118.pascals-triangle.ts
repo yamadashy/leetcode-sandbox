955d2f4084186fee410b5cb8e82cfaa2da032d5e
Kazuki Yamada <koukun0120@gmail.com>
2021-11-04 01:21:54 +0900

    add: 118

---
+++ solutions/0118.pascals-triangle.ts
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/pascals-triangle
+function generate(numRows: number): number[][] {
+  const numsArray: number[][] = [[1]];
+  let nums: number[] = [1];
+
+  if (numRows === 1) {
+    return [[1]];
+  }
+
+  for (let i = 1; i < numRows; i++) {
+    const numsLength = nums.length;
+    const nextNums: number[] = [1];
+
+    for (let j = 0; j < numsLength - 1; j++) {
+      const num = nums[j] + nums[j + 1];
+      nextNums.push(num);
+    }
+
+    nextNums.push(1);
+    numsArray.push(nextNums);
+    nums = nextNums;
+  }
+
+  return numsArray;
+}
