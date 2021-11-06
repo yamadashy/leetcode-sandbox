2a104b4b6c57f20cd90106019b2b8c40dcd5a442
Kazuki Yamada <koukun0120@gmail.com>
2021-11-06 23:48:30 +0900

    add: 119

---
+++ solutions/0119.pascals-triangle-ii.ts
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/pascals-triangle-ii
+function getRow(rowIndex: number): number[] {
+  let nums: number[] = [1];
+
+  for (let i = 1; i < rowIndex + 1; i++) {
+    const numsLength = nums.length;
+    const nextNums: number[] = [1];
+
+    for (let j = 0; j < numsLength - 1; j++) {
+      const num = nums[j] + nums[j + 1];
+      nextNums.push(num);
+    }
+
+    nextNums.push(1);
+    nums = nextNums;
+  }
+
+  return nums;
+};
