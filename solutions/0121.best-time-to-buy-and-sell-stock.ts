4987c0bde6704d88e5ad2c8718c5cbfa7aede138
Kazuki Yamada <koukun0120@gmail.com>
2021-11-07 22:34:26 +0900

    add: 121

---
+++ solutions/0121.best-time-to-buy-and-sell-stock.ts
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock
+function maxProfit(prices: number[]): number {
+  let minPrice = Infinity;
+  let maxDiff = 0;
+  const pricesLength = prices.length;
+
+  for (let i = 0; i < pricesLength; i++) {
+    const diff = prices[i] - minPrice;
+
+    if (prices[i] < minPrice) {
+      minPrice = prices[i];
+    }
+
+    if (maxDiff < diff) {
+      maxDiff = diff;
+    }
+  }
+
+  return maxDiff;
+};
