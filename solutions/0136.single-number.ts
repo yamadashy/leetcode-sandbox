f5819ca1725bac0573bd81acb04b3e556f5933c6
Kazuki Yamada <koukun0120@gmail.com>
2021-11-07 23:10:32 +0900

    add: 136

---
+++ solutions/0136.single-number.ts
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/single-number
+function singleNumber(nums: number[]): number {
+  const numAppearMap: boolean[] = [];
+
+  for (const num of nums) {
+    if (numAppearMap[num] === true) {
+      delete numAppearMap[num];
+    } else {
+      numAppearMap[num] = true;
+    }
+  }
+
+  return parseInt(Object.keys(numAppearMap)[0]);
+};
