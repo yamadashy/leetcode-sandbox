cf5146a52da2887cd8672489a30c4cda9d69e408
Kazuki Yamada <koukun0120@gmail.com>
2021-11-07 23:03:45 +0900

    add: 125

---
+++ solutions/0125.valid-palindrome.ts
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/valid-palindrome
+function isPalindrome(s: string): boolean {
+  const sLen = s.length;
+  const charList = [];
+
+  for (let i = 0; i < sLen; i++) {
+    if (/[a-zA-Z0-9]/.test(s[i])) {
+      charList.push(s[i].toLowerCase());
+    }
+  }
+
+  const charListLen = charList.length;
+  const charListHarlfLen = Math.ceil(charListLen / 2);
+  let isPalindrome = true;
+
+  for (let i = 0; i < charListHarlfLen; i++) {
+    if (charList[i] !== charList[charListLen - 1 - i]) {
+      isPalindrome = false;
+    }
+  }
+
+  return isPalindrome;
+};
