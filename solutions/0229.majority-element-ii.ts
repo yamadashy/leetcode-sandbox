8ebeb3aa446e377e6fd1f866e26a522fb32c1b1b
Kazuki Yamada <koukun0120@gmail.com>
2021-11-09 01:20:55 +0900

    add: 229

---
+++ solutions/0229.majority-element-ii.ts
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/majority-element-ii
+function majorityElement(nums: number[]): number[] {
+    const countMap: number[] = [];
+
+    nums.forEach((val) => {
+        if (!countMap[val]) {
+            countMap[val] = 0;
+        }
+        countMap[val]++;
+    });
+
+    const indexes: number[] = [];
+    const threshold = nums.length / 3;
+
+    for (const index in countMap) {
+        const count = countMap[index];
+        if (threshold < count) {
+            indexes.push(parseInt(index));
+        }
+    }
+
+    return indexes;
+};
+
+export {};
