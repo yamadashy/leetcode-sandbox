166df5964777ae481e8f543b2106b43d6024af32
Kazuki Yamada <koukun0120@gmail.com>
2021-11-09 00:52:47 +0900

    add: 168

---
+++ solutions/0168.excel-sheet-column-title.ts
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/excel-sheet-column-title
+function convertToTitle(columnNumber: number): string {
+    const charCodeA = 'A'.charCodeAt(0);
+    let title = '';
+
+    while (true) {
+        const mod = columnNumber % 26;
+        let charCode = charCodeA + mod - 1;
+
+        if (mod === 0) {
+            charCode = charCodeA + 25;
+            columnNumber -= 1;
+        }
+
+        title = String.fromCharCode(charCode) + title;
+        columnNumber = (columnNumber - mod) / 26;
+
+        if (columnNumber === 0) {
+            break;
+        }
+    }
+
+    return title;
+};
+
+export {};
