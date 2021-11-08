2a2df0342164c3a98dc1145948d48bc4e8586cb0
Kazuki Yamada <koukun0120@gmail.com>
2021-11-08 23:49:52 +0900

    add: 167

---
+++ solutions/0167.two-sum-ii-input-array-is-sorted.ts
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/two-sum-ii-input-array-is-sorted
+function twoSum(numbers: number[], target: number): number[] {
+    const numsLength = numbers.length;
+    let resultI = 0;
+    let resultJ = 0;
+
+    const numIndexMap = [];
+
+    for (let i = 0; i < numsLength; i++) {
+        numIndexMap[numbers[i]] = i;
+    }
+
+    for (let i = 0; i < numsLength; i++) {
+        const findNum = target - numbers[i];
+
+        if (numIndexMap[findNum] !== undefined) {
+            resultI = i;
+            resultJ = numIndexMap[findNum];
+            break;
+        }
+    }
+
+    return [resultI + 1, resultJ + 1];
+};
+
+export {};
