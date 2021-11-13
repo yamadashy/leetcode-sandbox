de2c26ccbff87988b26be4a16998896dcc8f05d7
Kazuki Yamada <koukun0120@gmail.com>
2021-11-13 19:22:48 +0900

    add: 703

---
+++ solutions/0703.kth-largest-element-in-a-stream.ts
@@ -0,0 +1,38 @@
+// https://leetcode.com/problems/kth-largest-element-in-a-stream
+class KthLargest {
+    private k: number;
+    private sortedNums: number[] = [];
+
+    constructor(k: number, nums: number[]) {
+        this.k = k;
+        this.sortedNums = nums.sort((a, b) => b - a);
+    }
+
+    add(val: number): number {
+        this.sortedNums.push(val);
+        // this.sortedNums.sort(sortFunction);
+        this.insertToSortedNums(val);
+        return this.sortedNums[this.k - 1];
+    }
+
+    private insertToSortedNums(val: number) {
+        const arrayLength = this.sortedNums.length;
+
+        for (let i = 0; i < arrayLength; i++) {
+            if (this.sortedNums[i] <= val) {
+                this.sortedNums.splice(i, 0, val);
+                return;
+            }
+        }
+
+        this.sortedNums.push(val);
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * var obj = new KthLargest(k, nums)
+ * var param_1 = obj.add(val)
+ */
+
+export {};
